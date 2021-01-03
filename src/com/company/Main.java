13aa3bae6f58f242d7cedaac2e42b77d9a5e6c19
Salimi 'C Ali <26351293+salimicali@users.noreply.github.com>
2021-01-04 00:46:07 +0330

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,15 @@
 package com.company;
 
 
-import java.awt.*;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javafx.application.Application;
+
+
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    new WindowFormMain();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+    public static void main(String[] args) {
+	// write your code here
+        Application.launch(MainForm.class);
     }
-
-    public static void SaveErrorLog(String msg) throws IOException {
-        PrintWriter writer = new PrintWriter(new FileWriter("error.log"));
-        writer.append(msg);
-        writer.close();
-    }
-
 }
+
